8028696cf95669b12ad52bea2ddc2ec5a2f27100
MrLaki5 <mimar1207@gmail.com>
2019-05-19 12:07:09 +0200

    wake on lan

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Socket data transfer:
+# Python-socket-usage
+
+## Socket data transfer:
 * Transfer images from server to client with sockets in python3.
+
+## Simple wake on lan:
+* Wake device in local network from another device.
+* Put mac address into wake_on_lan/mac_wakeup_addr.json in format: "FF:FF:FF:FF:FF:FF"
---
+++ wake_on_lan/magic_pakacge.py
@@ -0,0 +1,25 @@
+import socket
+import json
+
+with open("mac_wakeup_addr.json") as f:
+	mac_address = json.load(f)["mac_address"]
+mac_address_array = mac_address.split(":")
+
+# Mac address of machine that needs to be woken up
+mac_array = []
+for i in mac_address_array:
+	phase1 = int('0x' + i, 16)
+	mac_array.append(phase1)
+
+# Set up socket
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+server.bind(("", 44444))
+# Create wake on lan package (must be length of 110)
+arr = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
+for i in range(0, 16):
+	arr += mac_array
+message = bytes(arr)
+# Send package
+print("WOF message: " + str(message))
+server.sendto(message, ('<broadcast>', 9)) 
